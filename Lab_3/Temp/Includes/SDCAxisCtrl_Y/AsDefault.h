1036bb21f9a267732d47000442ccc6ac897ab364
Lagodrom <84673722+Lagodrom@users.noreply.github.com>
2023-12-19 14:54:23 +0300

    Delete Lab_3 directory

--- Lab_3/Temp/Includes/SDCAxisCtrl_Y/AsDefault.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SDCAXISCTRL_Y_DEFAULT_2023814119
-#define _SDCAXISCTRL_Y_DEFAULT_2023814119
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <variablesVAR.h>
-#endif
