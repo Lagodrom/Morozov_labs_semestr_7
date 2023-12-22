69a346fdcf81416c6d07aaed2dbe88ce5f508b2f
Lagodrom <84673722+Lagodrom@users.noreply.github.com>
2023-12-22 15:51:57 +0300

    Delete Lab4/coursework_Rectangle directory

--- Lab4/coursework_Rectangle/Temp/Includes/SDCAxisCtrl_X/AsDefault.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SDCAXISCTRL_X_DEFAULT_2023814118
-#define _SDCAXISCTRL_X_DEFAULT_2023814118
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <variablesVAR.h>
-#endif
--- Lab4/coursework_Rectangle/Temp/Includes/SDCAxisCtrl_Z/AsDefault.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SDCAXISCTRL_Z_DEFAULT_2023814120
-#define _SDCAXISCTRL_Z_DEFAULT_2023814120
-#include "../AsDefault.h"
-#include <bur/plctypes.h>
-#include <bur/plc.h>
-#include <variablesVAR.h>
-#endif
